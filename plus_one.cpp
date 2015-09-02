068c83149813d88c43dc4504e4417dc6342abb16
pyliu <pyliu@yahoo-inc.com>
2015-09-02 15:43:24 +0800

    use in-place change

--- plus_one.cpp
+++ plus_one.cpp
@@ -3,20 +3,18 @@
 
 using namespace std;
 
-vector<int> plusOne(vector<int> &digits) {
-    vector<int> res(digits.size(), 0);
+void plusOne(vector<int> &digits) {
     int sum = 0;
     int one = 1;
     for(int i =  digits.size() - 1; i >= 0; i--) {
         sum = one + digits[i];
         one = sum / 10;
-        res[i] = sum % 10;
+        digits[i] = sum % 10;
     }
 
     if(one > 0) {
-        res.insert(res.begin(), one);
+        digits.insert(digits.begin(), one);
     }
-    return res;
 }
 
 void print(vector<int>& in) {
@@ -30,8 +28,8 @@ void print(vector<int>& in) {
 int main() {
     vector<int> test = {1, 0, 0, 3};
     print(test);
-    auto result = plusOne(test);
-    print(result);
+    plusOne(test);
+    print(test);
     return 0;
 }
 
